c0548055d5a26c4ff6000277fcfe1236ae8563e2
daneArt <public.centaurus@gmail.com>
2020-08-11 21:29:05 +0300

    save changes

---
+++ lib/cubits/md_image/md_image_cubit.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mydeck/models/dtos/file_dto.dart';
+import 'package:mydeck/models/entitites/md_file.dart';
+import 'package:mydeck/models/entitites/unique_id.dart';
+import 'package:mydeck/services/usecases/get_file_by_meta_usecase.dart';
+
+part 'md_image_state.dart';
+part 'md_image_cubit.freezed.dart';
+
+class MDContentCubit extends Cubit<MDContentState> {
+  final GetFileByMetaUseCase _getFileByMetaUseCase;
+
+  MDContentCubit(this._getFileByMetaUseCase) : super(MDContentState.initial());
+
+  Future<void> initFile(MDFile sourceFile) async {
+    emit(MDContentState.loading());
+    final result = await sourceFile.getFileValue();
+    emit(
+      result.fold(
+        (error) => MDContentState.error(),
+        (file) => MDContentState.loaded(file),
+      ),
+    );
+  }
+}
---
+++ lib/cubits/md_image/md_image_state.dart
@@ -0,0 +1,9 @@
+part of 'md_image_cubit.dart';
+
+@freezed
+abstract class MDContentState with _$MDContentState {
+  factory MDContentState.initial() = MDImageStateInitial;
+  factory MDContentState.loading() = MDImageStateLoading;
+  factory MDContentState.error() = MDImageStateError;
+  factory MDContentState.loaded(File file) = MDImageStateLoaded;
+}
